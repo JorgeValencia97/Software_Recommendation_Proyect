49c8c373a69bd13a13703c94d4769d1ccf0de600
JorgeValencia97 <116290295+JorgeValencia97@users.noreply.github.com>
2023-02-03 13:14:03 -0500

    Uses ChatGPT to create a TreeNode Class

--- Colombia Travel Guide Recommendation Proyect.py
+++ Colombia Travel Guide Recommendation Proyect.py
@@ -1,17 +1,44 @@
 #Colombia Travel Guide Recommendation Proyect
 from TravelGuideData import travel_type
 from TravelGuideData import travel_destination
+from linkedlist import LinkedList
+from treenode import TreeNode
 
-
+#Create Greetings
 type_string = ""
 for type in travel_type:
     type_string += "{0}\n".format(type)
-
 
 def Colombia_travel_guide():
     print("*****Welcome to Colombia's paradise!*****\n")
     print("Prepare yourself for a wonderfull trip inside Colombia based on your interests")
     print("Type the beggining or whole word based on your favorite travel type:\n" + type_string)
 
+#Create a the initial tree childs based on travel_type
 
-Colombia_travel_guide()
+def build_tree(travel_destination):
+    root = TreeNode("Travel Destinations")
+    travel_type_map = {}
+    for dest in travel_destination:
+        travel_type, place, sub_destinations = dest[0], dest[1], dest[2:]
+        if travel_type not in travel_type_map:
+            travel_type_node = TreeNode(travel_type)
+            root.add_child(travel_type_node)
+            travel_type_map[travel_type] = travel_type_node
+        else:
+            travel_type_node = travel_type_map[travel_type]
+        place_node = TreeNode(place)
+        travel_type_node.add_child(place_node)
+        for sub_dest in sub_destinations:
+            sub_dest_node = TreeNode(sub_dest)
+            place_node.add_child(sub_dest_node)
+    return root
+
+root = build_tree(travel_destination)
+root.traverse()
+
+
+
+
+
+
--- TravelGuideData.py
+++ TravelGuideData.py
@@ -1,13 +1,13 @@
 travel_type = ["Beach", "Culture and sightseen", "Nature"]
 
 
-travel_destination = [["beach", "San Andrés Island", ["Posadas Nativas", "Providencia", "Seven Colors Sea", "Scuba Diving"]],
-                      ["beach", "Cartagena", ["Rosario Island", "Castillo De San Felipe", "Boca Grande"]],
-                      ["beach", "Santa Marta", ["Taganga", "Sierra Nevada De Santa Marta", "El Rodadero", "Tayrona Park"]],
-                      ["culture and sightseen", "Bogotá", ["Gold Museum", "Paloquemao Market", "Financial center", "Main Library"]],
-                      ["culture and sightseen", "Medellín", ["River Lights", "Etnographic museum", "Metrocable", "Butterfly Garden", "Metro De Medellín Trip"]],
-                      ["culture and sightseen", "Cali", ["Parks and Gardens", "River Avenue", "Dance Salsa"]],
-                      ["nature", "Amazonía", ["Rafting in Leticia", "Native's style Hotel", "Parque Ecológico", "Caño Cristales"]],
-                      ["nature", "Chocó", ["Whales watching", "Nuquí", "Manglares"]],
-                      ["nature", "Eje Cafetero", ["Parque del café", "Los Nevados Natural Park", "Archeological Park"]],
+travel_destination = [["Beach", "San Andrés Island", ["Posadas Nativas", "Providencia", "Seven Colors Sea", "Scuba Diving"]],
+                      ["Beach", "Cartagena", ["Rosario Island", "Castillo De San Felipe", "Boca Grande"]],
+                      ["Beach", "Santa Marta", ["Taganga", "Sierra Nevada De Santa Marta", "El Rodadero", "Tayrona Park"]],
+                      ["Culture and sightseen", "Bogotá", ["Gold Museum", "Paloquemao Market", "Financial center", "Main Library"]],
+                      ["Culture and sightseen", "Medellín", ["River Lights", "Etnographic museum", "Metrocable", "Butterfly Garden", "Metro De Medellín Trip"]],
+                      ["Culture and sightseen", "Cali", ["Parks and Gardens", "River Avenue", "Dance Salsa"]],
+                      ["Nature", "Amazonía", ["Rafting in Leticia", "Native's style Hotel", "Parque Ecológico", "Caño Cristales"]],
+                      ["Nature", "Chocó", ["Whales watching", "Nuquí", "Manglares"]],
+                      ["Nature", "Eje Cafetero", ["Parque del café", "Los Nevados Natural Park", "Archeological Park"]],
                       ]
