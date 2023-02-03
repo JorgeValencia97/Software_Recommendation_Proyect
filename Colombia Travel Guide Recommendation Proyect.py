0cce1e9f86a70d9ed5a07b4b8747da3574b4de9a
JorgeValencia97 <116290295+JorgeValencia97@users.noreply.github.com>
2023-02-03 11:08:55 -0500

    Create function and greetings

--- Colombia Travel Guide Recommendation Proyect.py
+++ Colombia Travel Guide Recommendation Proyect.py
@@ -1,2 +1,16 @@
 #Colombia Travel Guide Recommendation Proyect
+from TravelGuideData import travel_type
+from TravelGuideData import travel_destination
 
+type_string = ""
+for type in travel_type:
+    type_string += "{0}\n".format(type)
+
+
+def Colombia_travel_guide():
+    print("*****Welcome to Colombia's paradise!*****\n")
+    print("Prepare yourself for a wonderfull trip inside Colombia based on your interests")
+    print("Type the beggining or whole word based on your favorite travel type:\n" + type_string)
+
+
+Colombia_travel_guide()
